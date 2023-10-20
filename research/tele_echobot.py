e7bbb1a628ada27eef449130ed5d872a7d271b42
Dushyant kumar verma <91840332+dushyantver@users.noreply.github.com>
2023-10-20 18:58:24 +0530

    code commit

---
+++ research/tele_echobot.py
@@ -0,0 +1,34 @@
+import logging
+from aiogram import Bot, Dispatcher, types, executor
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_token = os.getenv("Token")
+#print(API_token)
+
+# Configuration
+logging.basicConfig(level=logging.INFO)
+
+# initialize bot and dispatecher
+bot = Bot(token = API_token)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def command_start_handler(message: types.Message):
+    """
+    This handler receives messages with `/start` or  `/help `command
+    """
+    await message.reply("Hi\nI am Echo Bot!\nPowered by aiogram.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+ 
+    """
+    This will retrun echo
+    """
+    await message.answer(message.text)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
